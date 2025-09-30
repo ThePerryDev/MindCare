9152a7d93e532a91cdfed09d7e10c03c80c83641
dani-dornas <lauby.dd@gmail.com>
2025-09-30 12:57:58 -0300

    correção de theme.ts

--- frontend/styles/theme.ts
+++ frontend/styles/theme.ts
@@ -2,7 +2,8 @@ export const theme = {
   fonts: {
     regular: 'Poppins_400Regular',
     medium: 'Poppins_500Medium',
-    semiBold: 'Poppins_600SemiBold',
+    semiBold: 'Poppins_600SemiBold', // mantém para compatibilidade
+    semi: 'Poppins_600SemiBold',
     bold: 'Poppins_700Bold',
   },
 };
