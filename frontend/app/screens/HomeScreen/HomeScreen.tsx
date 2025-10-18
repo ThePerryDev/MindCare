4e7a2761e8e675c95e16a6052d4916da513e74ff
Eduardo Moraes <luis270704@gmail.com>
2025-10-17 22:09:23 -0300

    Tela Home Screen e componentes finalizados

--- frontend/app/screens/HomeScreen/HomeScreen.tsx
+++ frontend/app/screens/HomeScreen/HomeScreen.tsx
@@ -1,21 +1,22 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import { SafeAreaView } from "react-native-safe-area-context";
-import Button from "@/components/Button/Button";
-import { styles } from "./styles";
-import WeeklyResults from "@/components/WeeklyResults/WeeklyResults";
-import ContinueImproving from "@/components/ContinueImproving/ContinueImproving";
-import Navbar from "@/components/Navbar/Navbar";
+import React from 'react';
+import { View, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Button from '@/components/Button/Button';
+import { styles } from './styles';
+import WeeklyResults from '@/components/WeeklyResults/WeeklyResults';
+import ContinueImproving from '@/components/ContinueImproving/ContinueImproving';
+import Navbar from '@/components/Navbar/Navbar';
 
 export default function HomeScreen() {
   const handleMoodPress = () => {
-    console.log("Botão de humor pressionado");
+    // eslint-disable-next-line no-console
+    console.log('Botão de humor pressionado');
   };
 
   return (
     <LinearGradient
-      colors={["#F4EFFF", "#D6F0F0"]}
+      colors={['#F4EFFF', '#D6F0F0']}
       start={{ x: 0, y: 0 }}
       end={{ x: 0, y: 1 }}
       style={styles.background}
